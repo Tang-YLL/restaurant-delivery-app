fe344070bf01a2396ca46f634cc4c85b1d4b483d
Tang-YLL <Tang-YLL@users.noreply.github.com>
2026-01-01 00:06:02 +0800

    Issue 012: 迁移UI页面层完成 - 迁移16个页面和4个组件

---
+++ flutter_app_new/lib/presentation/pages/category_page.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/product_provider.dart';
+import '../widgets/product_card.dart';
+
+/// 分类页面
+class CategoryPage extends StatefulWidget {
+  const CategoryPage({super.key});
+
+  @override
+  State<CategoryPage> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  String? _selectedCategory;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      context.read<ProductProvider>().loadCategories();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('商品分类'),
+      ),
+      body: Consumer<ProductProvider>(
+        builder: (context, provider, _) {
+          return Row(
+            children: [
+              // 左侧分类列表
+              SizedBox(
+                width: 100,
+                child: ListView.builder(
+                  itemCount: provider.categories.length + 1,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      // 全部分类
+                      return _buildCategoryItem(
+                        context,
+                        null,
+                        '全部',
+                        _selectedCategory == null,
+                      );
+                    }
+                    final category = provider.categories[index - 1];
+                    return _buildCategoryItem(
+                      context,
+                      category.name,
+                      category.name,
+                      _selectedCategory == category.name,
+                      icon: category.icon,
+                    );
+                  },
+                ),
+              ),
+              // 右侧商品列表
+              Expanded(
+                child: provider.isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : GridView.builder(
+                        padding: const EdgeInsets.all(12),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 0.85,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 12,
+                        ),
+                        itemCount: provider.products.length,
+                        itemBuilder: (context, index) {
+                          final product = provider.products[index];
+                          return ProductCard(product: product);
+                        },
+                      ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildCategoryItem(
+    BuildContext context,
+    String? category,
+    String label,
+    bool isSelected, {
+    String? icon,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selectedCategory = category;
+        });
+        context.read<ProductProvider>().filterByCategory(category);
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.orange.withOpacity(0.1) : Colors.white,
+          border: Border(
+            left: BorderSide(
+              color: isSelected ? Colors.orange : Colors.transparent,
+              width: 3,
+            ),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (icon != null)
+              Text(
+                icon,
+                style: const TextStyle(fontSize: 24),
+              )
+            else
+              Icon(
+                Icons.apps,
+                color: isSelected ? Colors.orange : Colors.grey,
+              ),
+            const SizedBox(height: 4),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                color: isSelected ? Colors.orange : Colors.grey[700],
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_app_new/lib/presentation/pages/main_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'cart_page.dart';
+import 'profile_page.dart';
+import 'order_list_page.dart';
+import 'category_page.dart';
+
+/// 主页面 - 带底部导航栏
+class MainPage extends StatefulWidget {
+  final int initialIndex;
+
+  const MainPage({super.key, this.initialIndex = 0});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late int _currentIndex;
+  final PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.initialIndex;
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  void _onTabTapped(int index) {
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: const [
+          HomePage(),
+          CategoryPage(),
+          CartPage(),
+          OrderListPage(),
+          ProfilePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '首页',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: '分类',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: '购物车',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: '订单',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '我的',
+          ),
+        ],
+      ),
+    );
+  }
+}
